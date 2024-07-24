b6a6f70aa83bb74d93e586beab5b3ecdb0f5accc
GroverSoans <groversoans@Grovers-Air.zyxel.com>
2024-07-24 16:15:16 -0500

    Added comments and changed grid

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def isValidMove(grid, row, col , num):
+    #Check if num is already in row
     for x in range(9):
         if grid[row][x] == num:
             return False
-        
+    #Check if num is already in col
     for x in range(9):
         if grid[x][col] == num:
             return False
@@ -10,12 +11,14 @@ def isValidMove(grid, row, col , num):
     cornerRow = row - row % 3
     cornerCol = col - col % 3
 
+    #Check if num is already in 3x3 grid
     for x in range(3):
         for y in range(3):
             if grid[cornerRow + x][cornerCol + y] == num:
                 return False
             
     return True
+
 
 def solve(grid, row, col):
 
@@ -31,9 +34,7 @@ def solve(grid, row, col):
     for num in range (1, 10):
 
         if isValidMove(grid, row, col, num):
-
             grid[row][col] = num
-
             if solve(grid, row, col +1):
                 return True
             
@@ -41,16 +42,17 @@ def solve(grid, row, col):
 
     return False
 
-grid = [[0, 0, 0, 0, 0, 0, 6, 8, 0],
-        [0, 0, 0, 0, 7, 3, 0, 0, 9],
-        [3, 0, 9, 0, 0, 0, 0, 4, 5],
-        [4, 9, 0, 0, 0, 0, 0, 0, 0],
-        [8, 0, 3, 0, 5, 0, 9, 0, 2],
-        [0, 0, 0, 0, 0, 0, 0, 3, 6],
-        [9, 6, 0, 0, 0, 0, 3, 0, 8],
-        [7, 0, 0, 6, 8, 0, 0, 0, 0],
-        [0, 2, 8, 0, 0, 0, 0, 0, 0]]
+grid = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 
+#Prints solution if there is one
 if solve(grid, 0, 0):
     for i in range(9):
         for j in range(9):
